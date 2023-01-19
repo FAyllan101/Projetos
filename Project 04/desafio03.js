e361f3f71805621196fdbe56d23af4afee115795
FAyllan101 <103951910+FAyllan101@users.noreply.github.com>
2023-01-19 18:00:13 -0300

    desafio03

--- Project 04/desafio03.js
+++ Project 04/desafio03.js
@@ -13,10 +13,35 @@
 //transformDegree('50F')
 
 function transformDegree(degree){
-    const celsiusExists = degree.toUpperCase.includes('C')
-    const farenheintExists = degree.toUpperCase.includes('F');
-
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const farenheintExists = degree.toUpperCase().includes('F');
+    // fluxo de erro
     if(!celsiusExists && !farenheintExists){
         throw new Error('Grau não identificado')
     }
+
+    // fluxo ideal F -> C
+    let updatedDegree = Number(degree.toUpperCase().replace("F","")); 
+    let formula = farenheit => (farenheit-32) * 5/9
+    let degreeSign = 'C'
+    
+    // fluxo alternativo C->F
+    if(celsiusExists){
+        updatedDegree = Number(degree.toUpperCase().replace("C","")); 
+        formula = celsius => (celsius) * 9/5 +32
+        degreeSign = 'F'
+    }
+
+    return formula(updatedDegree) + degreeSign
+
+}
+
+try {
+    console.log(transformDegree('50f'))
+    console.log(transformDegree('10c'))
+    transformDegree('50z')
+}
+
+catch (error){
+    console.log(error.message)
 }
