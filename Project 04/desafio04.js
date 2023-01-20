3271740eb8be58f5f56995062982a54b5df610b6
FAyllan101 <103951910+FAyllan101@users.noreply.github.com>
2023-01-20 15:59:24 -0300

    desafio04

--- Project 04/desafio04.js
+++ Project 04/desafio04.js
@@ -45,3 +45,44 @@ const booksByCategory = [
         ],
     },
 ];
+
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories);
+for(let category of booksByCategory){
+    console.log('Total de livros da categoria:', category.category)
+    console.log(category.books.length)
+}
+
+function countAutors(){
+    let authors = [];
+
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            if(authors.indexOf(book.author) == -1){
+                authors.push(book.author)
+            }
+            
+        }
+    }
+
+    console.log("Total de autores:", authors.length)
+}
+
+countAutors();
+
+function booksOfAuthor(author){
+    let books = [];
+
+    for (let category of booksByCategory){
+        for(let book of category.books){
+            if(book.author === author){
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+}
+
+booksOfAuthor("Augusto Cury")
